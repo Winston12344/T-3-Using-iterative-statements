412b3e1bcf185ad1b9be86c0be2e720110658393
Winston Leaphart <wleaphart@usf.edu>
2022-02-10 17:09:28 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// C# Program that converts CAD to US currency
+using System;
+class CAD_US
+{
+  
+// Main steps
+ public static void Main()
+{
+
+        //int variable for increament value for CAD in table
+        int inc_value;
+
+        // input inc_value between the range of 5 and 25
+        while (true)
+        {
+            // input
+            Console.Write("Enter value between 5 and 25 :");
+            inc_value = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        // variable for CAD an USD
+
+        double CAD = 0, USD;
+        
+        //heading labels for the table
+        Console.WriteLine
